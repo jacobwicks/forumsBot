3116027f05dc2c1b11ebc3f2ae4949e87d609286
jacobwicks <jacob.m.wicks@gmail.com>
2020-07-01 22:47:18 -0700

    upload instructions script

--- services/Config/Instructions/index.ts
+++ services/Config/Instructions/index.ts
@@ -1,8 +1,8 @@
 import { promises as fs } from 'fs';
 import * as path from 'path';
 import { replacer } from '../../JSONStringifyRegExp';
-import { exec } from 'child_process';
-import { series } from 'async';
+const { exec } = require('child_process');
+
 // const {exec} = require('child_process');
 
 // series([
@@ -18,9 +18,25 @@ export const saveInstructionsToFile = async (instructions: any) => {
     try {
         const instructionsString = JSON.stringify(instructions, replacer, 2);
         await fs.writeFile(instructionsPath, instructionsString, 'utf8');
-        exec('npm run uploadInstructions');
+        console.log('going execute script');
+        exec(
+            'npm run uploadInstructions',
+            (error: any, stdout: any, stderr: any) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            }
+        );
+
         return true;
     } catch (err) {
+        console.log(err);
         return false;
     }
 };
